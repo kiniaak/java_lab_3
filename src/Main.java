c5b6bca06399597beca2bc09ef87e7dd9f81f704
kiniaak <88184781+kiniaak@users.noreply.github.com>
2021-11-28 21:08:19 +0100

    ćwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+          Student Piotr=new Student();
+          Student Kamil=new Student();
+          Student Antek=new Student();
+          Student[]tab=new Student[3];
+          tab[0]=Piotr;
+          tab[1]=Kamil;
+          tab[2]=Antek;
 
+        for(int i=0;i<tab.length;i++)
+        {
+            System.out.println(tab[i]);
+        }
         /* ćw 2 klasy */
         /* zad.1
             a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
