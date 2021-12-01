65706ca42eba60912dc66104032bff050d893c04
kiniaak <88184781+kiniaak@users.noreply.github.com>
2021-12-01 16:33:41 +0100

    ćwiczenie 5

--- src/Samochod.java
+++ src/Samochod.java
@@ -3,4 +3,10 @@ String marka;
 String kolor;
 Integer przebieg;
 
+    public Samochod(String marka,String kolor,Integer przebieg) {
+        this.marka = marka;
+        this.kolor=kolor;
+        this.przebieg=przebieg;
+    }
+
 }
